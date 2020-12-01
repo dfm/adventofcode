effc058545df37dfcc73fd7ee52ec8e5de0858e0
Dan F-M <foreman.mackey@gmail.com>
2020-12-01 17:49:18 -0500

    don't run the days

--- days/Day01.hs
+++ days/Day01.hs
@@ -6,8 +6,5 @@ import Advent.Day01 (part1, part2)
 main :: IO ()
 main = do
   numbers <- loadListOfInts "inputs/day01"
-  putStrLn "Day 1:"
-  putStr " - Part 1: "
   print $ part1 numbers
-  putStr " - Part 2: "
   print $ part2 numbers
