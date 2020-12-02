18c3f06abcdc0569b2fb6badde328fdf309bf2d3
Dan F-M <foreman.mackey@gmail.com>
2020-12-01 19:30:36 -0500

    better scaling

--- src/Advent/Day01.hs
+++ src/Advent/Day01.hs
@@ -1,9 +1,12 @@
 module Advent.Day01 (part1, part2) where
 
-import Data.List (tails)
+import Data.List (sort, tails)
+
+findFirst :: (Ord a, Num a) => a -> [a] -> [a]
+findFirst x sortedNumbers = take 1 [y | y <- sortedNumbers, x + y >= 2020]
 
 part1 :: [Integer] -> Integer
-part1 numbers = head [x * y | (x : ys) <- tails numbers, y <- ys, x + y == 2020]
+part1 numbers = head $ take 1 [x * y | (x : ys) <- tails $ sort numbers, y <- findFirst x ys, x + y == 2020]
 
 part2 :: [Integer] -> Integer
-part2 numbers = head [x * y * z | (x : ys) <- tails numbers, (y : zs) <- tails ys, z <- zs, x + y + z == 2020]
+part2 numbers = head $ take 1 [x * y * z | (x : ys) <- tails $ sort numbers, (y : zs) <- tails ys, z <- findFirst (x + y) zs, x + y + z == 2020]
