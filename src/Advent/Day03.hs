867b339ae711097bd664cf2dae35fe7e77227f0e
Dan F-M <foreman.mackey@gmail.com>
2020-12-03 08:43:46 -0500

    nicer

--- src/Advent/Day03.hs
+++ src/Advent/Day03.hs
@@ -1,30 +1,30 @@
 module Advent.Day03 (part1, part2) where
 
-data TreeMap = TreeMap Int Int [Bool] deriving (Show)
+newtype TreeMap = TreeMap (Int, [Bool]) deriving (Show)
 
 part1 :: String -> Int
 part1 inp =
-  let dx = 3
-      dy = 1
-      treeMap = readTreeMap inp
-   in move treeMap dx dy 0 0 0
+  let TreeMap (stride, vs) = readTreeMap inp
+   in countTrees vs stride 3 1
 
 part2 :: String -> Int
 part2 inp =
-  let treeMap = readTreeMap inp
-   in product $ [move treeMap dx dy 0 0 0 | (dx, dy) <- zip [1, 3, 5, 7, 1] [1, 1, 1, 1, 2]]
+  let TreeMap (stride, vs) = readTreeMap inp
+   in product $ [countTrees vs stride dx dy | (dx, dy) <- zip [1, 3, 5, 7, 1] [1, 1, 1, 1, 2]]
 
 readTreeMap :: String -> TreeMap
 readTreeMap text =
   let inp = lines text
-      rows = length inp
       cols = length $ head inp
-   in TreeMap rows cols $ map (== '#') (concat inp)
+   in TreeMap (cols, map (== '#') (concat inp))
 
-move :: TreeMap -> Int -> Int -> Int -> Int -> Int -> Int
-move tree@(TreeMap rows cols inp) dx dy x y count
-  | y >= rows = count
-  | otherwise = move tree dx dy ((x + dx) `mod` cols) (y + dy) newCount
-  where
-    ind = x + y * cols
-    newCount = if inp !! ind then count + 1 else count
+countTrees :: [Bool] -> Int -> Int -> Int -> Int
+countTrees vs stride dx dy = length $ filter id (sled vs stride dx dy 0 0)
+
+sled :: [Bool] -> Int -> Int -> Int -> Int -> Int -> [Bool]
+sled [] _ _ _ _ _ = []
+sled (v : vs) stride dx dy x y =
+  let x' = (x + dx) `mod` stride
+      y' = y + dy
+      delta = x' - x + dy * stride - 1
+   in v : sled (drop delta vs) stride dx dy x' y'
