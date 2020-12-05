60cb0706e4f1506a431e4777f4fff3e427ec9605
Dan F-M <foreman.mackey@gmail.com>
2020-12-05 10:29:39 -0500

    cleaner

--- src/Advent/Day05.hs
+++ src/Advent/Day05.hs
@@ -1,16 +1,17 @@
-module Advent.Day05 where
+module Advent.Day05 (part1, part2) where
 
 import Data.Sort (sort)
 import Numeric (readInt)
 
 part1 :: String -> Int
-part1 text = maximum $ map readId (lines text)
+part1 = maximum . map readId . lines
 
 part2 :: String -> Int
-part2 text =
-  let ids = sort $ map readId (lines text)
-      diffs = filter (\(x, y) -> y - x == 2) $ zip ids (drop 1 ids)
-   in 1 + fst (head diffs)
+part2 = (+ 1) . head . findGaps . sort . map readId . lines
 
 readId :: String -> Int
 readId = fst . head . readInt 2 (`elem` "FBLR") (fromEnum . (`elem` "BR"))
+
+findGaps :: [Int] -> [Int]
+findGaps (x : y : zs) = if y - x == 2 then x : findGaps (y : zs) else findGaps (y : zs)
+findGaps _ = []
