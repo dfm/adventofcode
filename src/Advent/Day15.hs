d7eb0fdd5bf084cc5baa550825b1c4bd15f13b21
Dan F-M <foreman.mackey@gmail.com>
2020-12-15 12:44:58 -0500

    simpler day 15

--- src/Advent/Day15.hs
+++ src/Advent/Day15.hs
@@ -8,13 +8,13 @@ import Data.List.Split (splitOn)
 part1 :: Bool -> String -> Int
 part1 _ text =
   let input = parseInput text
-      result = foldl' (\state _ -> getNextNumber state) input [sTurn input .. 2020 - 1]
+      result = foldl' (\state _ -> getNextNumber state) input [sTurn input .. 2020 - 2]
    in sNumber result
 
 part2 :: Bool -> String -> Int
 part2 _ text =
   let input = parseInput text
-      result = foldl' (\state _ -> getNextNumber state) input [sTurn input .. 30000000 - 1]
+      result = foldl' (\state _ -> getNextNumber state) input [sTurn input .. 30000000 - 2]
    in sNumber result
 
 data State = State {sTurn :: Int, sNumber :: Int, sPast :: IntMap Int} deriving (Show)
@@ -23,13 +23,13 @@ parseInput :: String -> State
 parseInput text =
   let numbers = map read $ splitOn "," text
    in State
-        { sTurn = length numbers,
+        { sTurn = length numbers - 1,
           sNumber = last numbers,
           sPast = IM.fromList $ zip (init numbers) [0 ..]
         }
 
 getNextNumber :: State -> State
 getNextNumber (State turn current past) =
-  let n = if IM.member current past then turn - 1 - past IM.! current else 0
-      newPast = IM.insert current (turn - 1) past
+  let n = turn - IM.findWithDefault turn current past
+      newPast = IM.insert current turn past
    in State {sTurn = turn + 1, sNumber = n, sPast = newPast}
