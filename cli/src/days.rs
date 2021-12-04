849fd80aeb7748627f83dece4bca7f8f9a2a3a7c
Dan F-M <foreman.mackey@gmail.com>
2021-12-04 09:16:35 -0500

    adding boilerplate for day 5

--- cli/src/days.rs
+++ cli/src/days.rs
@@ -6,9 +6,10 @@ use aoc_day01::Day01;
 use aoc_day02::Day02;
 use aoc_day03::Day03;
 use aoc_day04::Day04;
+use aoc_day05::Day05;
 // __USE
 
-pub const MAX_DAY: u8 = 4;
+pub const MAX_DAY: u8 = 5;
 
 macro_rules! run_solver {
     ( $day:tt, $data:expr ) => {
@@ -34,6 +35,7 @@ pub fn run_day(day: u8) -> Result<()> {
         2 => run_solver!(Day02, handler.into()),
         3 => run_solver!(Day03, handler.into()),
         4 => run_solver!(Day04, handler.into()),
+        5 => run_solver!(Day05, handler.into()),
         // __MATCH
         _ => Err(aoc::Error::NotImplemented.into()),
     }
---
+++ days/day05/src/lib.rs
@@ -0,0 +1,25 @@
+use anyhow::Result;
+use aoc::solver::Solver;
+
+pub struct Day05;
+
+impl Solver<&str> for Day05 {
+    fn part1(_data: &str) -> Result<String> {
+        Err(aoc::Error::NotImplemented.into())
+    }
+
+    fn part2(_data: &str) -> Result<String> {
+        Err(aoc::Error::NotImplemented.into())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part1() {}
+
+    #[test]
+    fn test_part2() {}
+}
