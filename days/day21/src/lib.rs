b0b1649e4adc557bf506b5e4bf3c7312d99531d3
Dan F-M <foreman.mackey@gmail.com>
2021-12-21 20:30:25 -0500

    cleanup

--- days/day21/src/lib.rs
+++ days/day21/src/lib.rs
@@ -3,97 +3,82 @@ use std::collections::HashMap;
 
 pub struct Day21;
 
-type State = (u8, usize);
+type State = (usize, usize);
+type FullState = (usize, usize, HashMap<State, usize>);
 
-fn parse(data: &str) -> (u8, u8) {
-    let parse_one = |line: &str| line.split(": ").skip(1).next().unwrap().parse().unwrap();
+fn parse(data: &str) -> (State, State) {
+    let parse_one = |line: &str| line.split(": ").nth(1).unwrap().parse().unwrap();
     let mut lines = data.lines();
     (
-        parse_one(lines.next().unwrap()),
-        parse_one(lines.next().unwrap()),
+        (parse_one(lines.next().unwrap()), 0),
+        (parse_one(lines.next().unwrap()), 0),
     )
 }
 
-fn step1(rolls: &mut impl Iterator<Item = usize>, (current, score): State) -> State {
-    let delta = rolls.next().unwrap() + rolls.next().unwrap() + rolls.next().unwrap();
-    let next = ((current - 1) as usize + delta) % 10 + 1;
-    (next as u8, score + next)
+fn step((position, score): &State, delta: usize) -> State {
+    let next = ((position - 1) as usize + delta) % 10 + 1;
+    (next, score + next)
 }
 
-fn step2(
+fn advance1(rolls: &mut impl Iterator<Item = usize>, state: &State) -> State {
+    let delta = rolls.next().unwrap() + rolls.next().unwrap() + rolls.next().unwrap();
+    step(state, delta)
+}
+
+fn advance_all(
     states: &mut HashMap<State, usize>,
-    ((current, score), weight): (&State, &usize),
+    (state, weight): (&State, &usize),
 ) -> (usize, usize) {
     let mut count = 0;
     let mut wins = 0;
-    for (d, n) in [(3, 1), (4, 3), (5, 6), (6, 7), (7, 6), (8, 3), (9, 1)] {
-        let next = ((current - 1) as usize + d) % 10 + 1;
-        if score + next >= 21 {
+    for (delta, n) in [(3, 1), (4, 3), (5, 6), (6, 7), (7, 6), (8, 3), (9, 1)] {
+        let new_state = step(state, delta);
+        if new_state.1 >= 21 {
             wins += n * weight;
         } else {
-            states.increment((next as u8, score + next), weight * n);
+            states.increment(new_state, weight * n);
             count += n * weight;
         }
     }
     (count, wins)
 }
 
+fn advance2(count: usize, (_, wins, states): &FullState) -> FullState {
+    let mut tmp = HashMap::new();
+    let result = states.iter().fold((0, 0), |(total, wins), x| {
+        let (all, more_wins) = advance_all(&mut tmp, x);
+        (total + all, wins + more_wins)
+    });
+    (result.0, wins + count * result.1, tmp)
+}
+
 impl Solver<&str> for Day21 {
     fn part1(data: &str) -> usize {
-        let mut s1 = 0;
-        let mut s2 = 0;
-        let (mut p1, mut p2) = parse(data);
-        let mut rolls = (0usize..).map(|n| n % 100 + 1);
         let mut count = 0;
-        loop {
-            let result = step1(&mut rolls, (p1, s1));
-            p1 = result.0;
-            s1 = result.1;
-            count += 3;
-            if s1 >= 1000 {
-                return s2 * count;
+        let mut rolls = (0usize..).map(|n| n % 100 + 1);
+        let (mut s1, mut s2) = parse(data);
+        while (s1.1 < 1000) && (s2.1 < 1000) {
+            if count % 2 == 0 {
+                s1 = advance1(&mut rolls, &s1);
+            } else {
+                s2 = advance1(&mut rolls, &s2);
             }
-
-            let result = step1(&mut rolls, (p2, s2));
-            p2 = result.0;
-            s2 = result.1;
-            count += 3;
-            if s2 >= 1000 {
-                return s1 * count;
-            }
+            count += 1;
         }
+        std::cmp::min(s1.1, s2.1) * count * 3
     }
 
     fn part2(data: &str) -> usize {
-        let mut num2 = 1;
-        let mut w1 = 0;
-        let mut w2 = 0;
-        let (p1, p2) = parse(data);
-        let mut s1 = HashMap::new();
-        s1.insert((p1, 0), 1);
-        let mut s2 = HashMap::new();
-        s2.insert((p2, 0), 1);
-
+        let (s1, s2) = parse(data);
+        let mut state1 = (1, 0, HashMap::new());
+        state1.2.insert(s1, 1);
+        let mut state2 = (1, 0, HashMap::new());
+        state2.2.insert(s2, 1);
         for _ in 0..10 {
-            let mut tmp = HashMap::new();
-            let (num1, dw) = s1.iter().fold((0, 0), |(l, w), x| {
-                let (dl, dw) = step2(&mut tmp, x);
-                (l + dl, w + dw)
-            });
-            w1 += num2 * dw;
-            s1 = tmp;
-
-            let mut tmp = HashMap::new();
-            let result = s2.iter().fold((0, 0), |(l, w), x| {
-                let (dl, dw) = step2(&mut tmp, x);
-                (l + dl, w + dw)
-            });
-            num2 = result.0;
-            w2 += num1 * result.1;
-            s2 = tmp;
+            state1 = advance2(state2.0, &state1);
+            state2 = advance2(state1.0, &state2);
         }
-
-        std::cmp::max(w1, w2)
+        std::cmp::max(state1.1, state2.1)
     }
 }
 
