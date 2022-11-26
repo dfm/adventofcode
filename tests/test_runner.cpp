70fbdb0f578fa0b247270b39e227a9a9fbdb4402
Dan F-M <foreman.mackey@gmail.com>
2022-11-26 18:30:26 -0500

    debugginh

--- tests/test_runner.cpp
+++ tests/test_runner.cpp
@@ -11,9 +11,9 @@ TEST_CASE("[runner]") {
   using Out = int;
   auto part1 = [](In in) -> Out { return 1; };
   auto part2 = [](In in) -> Out { return 2; };
-  // aoc::registry::register_implementation(2021, 1, part1, part2);
-  // std::istringstream in("hello");
-  // std::ostringstream out;
+  aoc::registry::register_implementation(2021, 1, part1, part2);
+  std::istringstream in("hello");
+  std::ostringstream out;
   // aoc::run_with_data(2021, 1, in, out);
   // REQUIRE(out.str() ==
   //         std::string("* Dec 1, 2021 *\n=> Part 1: 1\n=> Part 2: 2\n"));
