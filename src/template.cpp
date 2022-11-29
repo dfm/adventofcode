bd0869c9eead3704c8d1a5a76b8b35319c22de2b
Dan F-M <foreman.mackey@gmail.com>
2022-11-29 11:24:39 -0500

    has concepts

--- src/template.cpp
+++ src/template.cpp
@@ -2,8 +2,6 @@
 
 namespace {
 
-namespace grammar {
-
 namespace dsl = lexy::dsl;
 typedef std::int64_t T;
 
@@ -17,14 +15,12 @@ struct parser {
   static constexpr auto value = lexy::as_list<std::vector<T>>;
 };
 
-}  // namespace grammar
-
 auto part1 = [](auto) { return 0; };
 
 auto part2 = [](auto) { return 0; };
 
 }  // namespace
 
-AOC_REGISTER({{year}}, {{day}}, grammar::parser, part1, part2);
+AOC_REGISTER({{year}}, {{day}}, parser, part1, part2);
 AOC_TEST_CASE(part1, "0", 0)
 AOC_TEST_CASE(part2, "0", 0)
