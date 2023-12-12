a724c9fe65419f7370c0f15f097adb08befed968
Dan F-M <foreman.mackey@gmail.com>
2023-12-12 11:12:39 -0500

    12 - ugh

---
+++ aoc/src/y2023/d12.rs
@@ -0,0 +1,121 @@
+use std::collections::HashMap;
+
+#[derive(Debug, Clone)]
+pub struct Data {
+  data: String,
+  groups: Vec<usize>,
+}
+
+pub fn parse(data: &str) -> Vec<Data> {
+  data
+    .lines()
+    .map(|line| {
+      let (a, b) = line.split_once(' ').unwrap();
+      // let data = a.replace('.', " ");
+      let groups = b.split(',').map(|s| s.parse().unwrap()).collect();
+      Data {
+        data: a.to_string(),
+        groups,
+      }
+    })
+    .collect()
+}
+
+pub fn part1(data: &[Data]) -> usize {
+  data
+    .iter()
+    .map(|d| {
+      let mut cache = HashMap::new();
+      solve(d.data.as_bytes(), None, &d.groups, &mut cache)
+    })
+    .sum()
+}
+
+pub fn part2(data: &[Data]) -> usize {
+  data
+    .iter()
+    .map(|d| {
+      let mut groups = d.groups.clone();
+      let mut data = d.data.clone();
+      for _ in 1..5 {
+        groups.append(&mut d.groups.clone());
+        data.push_str(&("?".to_owned() + &d.data));
+      }
+      let mut cache = HashMap::new();
+      solve(data.as_bytes(), None, &groups, &mut cache)
+    })
+    .sum()
+}
+
+fn solve(
+  data: &[u8],
+  current: Option<usize>,
+  groups: &[usize],
+  cache: &mut HashMap<String, usize>,
+) -> usize {
+  let key = format!(
+    "{}, {:?}, {:?}",
+    std::str::from_utf8(data).unwrap(),
+    current,
+    groups
+  );
+
+  if let Some(v) = cache.get(&key) {
+    return *v;
+  }
+
+  let v = if data.is_empty() {
+    // Finished
+    match (current, groups) {
+      (Some(n), &[m]) => (n == m) as usize,
+      (None, &[]) => 1,
+      _ => 0,
+    }
+  } else {
+    match (data[0], current, groups) {
+      // No group
+      (b'.', None, _) => solve(&data[1..], None, groups, cache),
+      // New group
+      (b'#', None, [_, ..]) => solve(&data[1..], Some(1), groups, cache),
+      // Accumulate group
+      (b'#', Some(n), _) => solve(&data[1..], Some(n + 1), groups, cache),
+      // Accumulate group on question mark
+      (b'?', Some(n), &[g, ..]) if n < g => solve(&data[1..], Some(n + 1), groups, cache),
+      // End group
+      (b'.' | b'?', Some(n), &[g, ..]) if g == n => solve(&data[1..], None, &groups[1..], cache),
+      // Branch
+      (b'?', None, _) => {
+        solve(&data[1..], Some(1), groups, cache) + solve(&data[1..], None, groups, cache)
+      }
+      _ => 0,
+    }
+  };
+
+  cache.insert(key, v);
+  v
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  const TEST_DATA: &str = r"???.### 1,1,3
+.??..??...?##. 1,1,3
+?#?#?#?#?#?#?#? 1,3,1,6
+????.#...#... 4,1,1
+????.######..#####. 1,6,5
+?###???????? 3,2,1
+";
+
+  #[test]
+  fn test_part1() {
+    let data = parse(TEST_DATA);
+    assert_eq!(part1(&data), 21);
+  }
+
+  #[test]
+  fn test_part2() {
+    let data = parse(TEST_DATA);
+    assert_eq!(part2(&data), 525152);
+  }
+}
