ba4efdf079192199505c083f8dc500281ec027a1
Dan F-M <foreman.mackey@gmail.com>
2023-12-16 08:51:42 -0500

    cleanup

--- aoc/src/y2023/d16.rs
+++ aoc/src/y2023/d16.rs
@@ -2,48 +2,47 @@ use crate::helpers::CharGrid;
 use rayon::prelude::*;
 use std::collections::HashSet;
 
-#[derive(Copy, Clone, Debug, Hash, Eq, PartialEq)]
-enum Dir {
-  Right,
-  Left,
-  Up,
-  Down,
-}
-
-fn propagate(grid: &CharGrid, start: (i64, i64, Dir), visited: &mut HashSet<(i64, i64, Dir)>) {
-  let (mut x, mut y, mut d) = start;
-  while grid.in_bounds(x, y) && !visited.contains(&(x, y, d)) {
-    visited.insert((x, y, d));
+fn propagate(
+  grid: &CharGrid,
+  start: (i64, i64, i64, i64),
+  visited: &mut HashSet<(i64, i64, i64, i64)>,
+) {
+  let (mut x, mut y, mut dx, mut dy) = start;
+  while grid.in_bounds(x, y) && !visited.contains(&(x, y, dx, dy)) {
+    visited.insert((x, y, dx, dy));
     let value = grid.get(x as usize, y as usize);
-    if matches!((value, d), (b'|', Dir::Right | Dir::Left)) {
-      propagate(grid, (x, y + 1, Dir::Down), visited);
-      propagate(grid, (x, y - 1, Dir::Up), visited);
+    if value == b'|' && dx != 0 {
+      propagate(grid, (x, y + 1, 0, 1), visited);
+      propagate(grid, (x, y - 1, 0, -1), visited);
       return;
     }
-    if matches!((value, d), (b'-', Dir::Up | Dir::Down)) {
-      propagate(grid, (x + 1, y, Dir::Right), visited);
-      propagate(grid, (x - 1, y, Dir::Left), visited);
+    if value == b'-' && dy != 0 {
+      propagate(grid, (x + 1, y, 1, 0), visited);
+      propagate(grid, (x - 1, y, -1, 0), visited);
       return;
     }
-    (x, y, d) = match (value, d) {
-      (b'\\', Dir::Right) | (b'/', Dir::Left) => (x, y + 1, Dir::Down),
-      (b'\\', Dir::Left) | (b'/', Dir::Right) => (x, y - 1, Dir::Up),
-      (b'\\', Dir::Up) | (b'/', Dir::Down) => (x - 1, y, Dir::Left),
-      (b'\\', Dir::Down) | (b'/', Dir::Up) => (x + 1, y, Dir::Right),
-      (_, Dir::Right) => (x + 1, y, Dir::Right),
-      (_, Dir::Left) => (x - 1, y, Dir::Left),
-      (_, Dir::Up) => (x, y - 1, Dir::Up),
-      (_, Dir::Down) => (x, y + 1, Dir::Down),
+    (x, y, dx, dy) = match (value, dx, dy) {
+      (b'\\', 1, 0) | (b'/', -1, 0) => (x, y + 1, 0, 1),
+      (b'\\', -1, 0) | (b'/', 1, 0) => (x, y - 1, 0, -1),
+      (b'\\', 0, -1) | (b'/', 0, 1) => (x - 1, y, -1, 0),
+      (b'\\', 0, 1) | (b'/', 0, -1) => (x + 1, y, 1, 0),
+      _ => (x + dx, y + dy, dx, dy),
     };
   }
 }
 
-fn energized(visited: &HashSet<(i64, i64, Dir)>) -> usize {
+fn energized(visited: &HashSet<(i64, i64, i64, i64)>) -> usize {
   visited
     .iter()
-    .map(|(x, y, _)| (x, y))
+    .map(|(x, y, _, _)| (x, y))
     .collect::<HashSet<_>>()
     .len()
+}
+
+fn solve(grid: &CharGrid, start: (i64, i64, i64, i64)) -> usize {
+  let mut visited = HashSet::new();
+  propagate(grid, start, &mut visited);
+  energized(&visited)
 }
 
 pub fn parse(data: &str) -> CharGrid {
@@ -51,48 +50,24 @@ pub fn parse(data: &str) -> CharGrid {
 }
 
 pub fn part1(data: &CharGrid) -> usize {
-  let mut visited = HashSet::new();
-  propagate(data, (0, 0, Dir::Right), &mut visited);
-  energized(&visited)
+  solve(data, (0, 0, 1, 0))
 }
 
 pub fn part2(data: &CharGrid) -> usize {
   let max_x = (0..data.width)
     .into_par_iter()
     .map(|x| {
-      let mut visited = HashSet::new();
-      propagate(data, (x as i64, 0, Dir::Down), &mut visited);
-      let value = energized(&visited);
-
-      visited.clear();
-      propagate(
-        data,
-        (x as i64, (data.height - 1) as i64, Dir::Up),
-        &mut visited,
-      );
-      value.max(energized(&visited))
+      solve(data, (x as i64, 0, 0, 1)).max(solve(data, (x as i64, (data.height - 1) as i64, 0, -1)))
     })
     .max()
     .unwrap();
-
   let max_y = (0..data.height)
     .into_par_iter()
     .map(|y| {
-      let mut visited = HashSet::new();
-      propagate(data, (0, y as i64, Dir::Right), &mut visited);
-      let value = energized(&visited);
-
-      visited.clear();
-      propagate(
-        data,
-        ((data.width - 1) as i64, y as i64, Dir::Left),
-        &mut visited,
-      );
-      value.max(energized(&visited))
+      solve(data, (0, y as i64, 1, 0)).max(solve(data, ((data.width - 1) as i64, y as i64, -1, 0)))
     })
     .max()
     .unwrap();
-
   max_x.max(max_y)
 }
 
